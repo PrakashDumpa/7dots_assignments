0d34dbae1793cf1377371ca296c0b90e0823d59c
Prakash Dumpa <prakashdumpa143@gmail.com>
2024-05-17 18:39:36 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+import React, { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const totalSeconds = 180;
+  const [time, setTime] = useState(totalSeconds);
+  const [isStop, setIsStop] = useState(true);
+
+  useEffect(() => {
+    if (time === 0) return;
+    let interval;
+    if (isStop) {
+      interval = setInterval(() => {
+        setTime((prev) => prev - 1);
+      }, 1000);
+    }
+    return () => clearInterval(interval);
+  }, [isStop, time]);
+
+  const onClickDecrease = () => {
+    if (time > 10) {
+      setTime((prev) => prev - 10);
+    }
+  };
+
+  const onClickIncrease = () => {
+    if (time <= totalSeconds - 10) setTime((prev) => prev + 10);
+  };
+
+  const minutes = Math.floor(time / 60);
+  const remainingSeconds = time % 60;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+      <div>
+        <CircularProgressbar
+          value={time}
+          maxValue={totalSeconds}
+          text={`${String(minutes).padStart(2, 0)} :
+        ${String(remainingSeconds).padStart(2, 0)}`}
+        />
+      </div>
+      {/* <h1>
+        {String(minutes).padStart(2, 0)} :
+        {String(remainingSeconds).padStart(2, 0)}
+      </h1> */}
+      <div className="w-50 mt-3 d-flex justify-content-around">
+        <button
+          type="btn"
+          className="btn btn-primary px-5"
+          onClick={onClickDecrease}
         >
-          Learn React
-        </a>
-      </header>
+          -
+        </button>
+        <button
+          type="btn"
+          className="btn btn-success px-5"
+          onClick={onClickIncrease}
+        >
+          +
+        </button>
+        <button
+          type="btn"
+          className="btn btn-danger px-4"
+          onClick={() => setIsStop((prev) => !prev)}
+        >
+          pause
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
